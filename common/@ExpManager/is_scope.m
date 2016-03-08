5fdf1474d1afafee7e1a3145560cb7bdd4f9a448
Rob McGurrin <rmcgurrin@bbn.com>
2016-03-07 21:24:22 -0500

    remove old alazar driver

--- common/@ExpManager/is_scope.m
+++ common/@ExpManager/is_scope.m
@@ -1,5 +1,5 @@
 function out = is_scope(instr)
-    out = ismember(class(instr), {'deviceDrivers.AgilentAP240', 'deviceDrivers.AlazarATS9870', 'X6'});
+    out = ismember(class(instr), {'deviceDrivers.AgilentAP240', 'AlazarATS9870', 'X6'});
     out = out || (isprop(instr, 'is_scope') && instr.is_scope);
 end
 
